5a635946d364d02972b986f850e187b55f9de31c
AlejandroChavezA00571497 <alejandrochavez108@outlook.com>
2023-09-08 09:58:23 -0600

    Bar Chart and Bubble Chart functions almost ready

--- static/js/app.js
+++ static/js/app.js
@@ -7,5 +7,82 @@ d3.json(url).then(function(jsonData){
 //According to the result, we have names, metadata and samples as available arrays
 
 
+//Reading the data, extracting the first subject's data and using it to run the inital bar chart function
+d3.json(url).then(function(jsonData){
+    let subjectID = jsonData.names[0];
+    initialChart(subjectID)
+});
 
+// Define function for the initial chart
+function initialChart(subjectID){
+    d3.json(url).then(function(jsonData){
+        let subjectSelector = d3.select("#selDataset");
+        let subjectNames = jsonData.names;
+        subjectNames.forEach(function(subjectName){
+            subjectSelector.append("option").text(subjectName);
+        })
+    
+        barChart(subjectID);
+        bubbleChart(subjectID);
+        demographicInfo(subjectID);
+
+    })
+}
+
+
+//Define function for building the barCharts
+function barChart(subjectID){
+    d3.json(url).then(function(jsonData){
+        let subjectData = jsonData.samples.filter(x => x.id == subjectID);
+
+
+        let barChartData = [{
+            x : subjectData[0].sample_values.slice(0,10).reverse(),
+            y : subjectData[0].otu_ids.slice(0, 10).reverse(),
+            type : "bar",
+            orientation : "h",
+            text : subjectData[0].otu_labels.slice(0,10).reverse()
+        }];
+
+        let barChartLayout = {
+            height : 500,
+            width : 600,
+            yaxis : {type : "category"}
+        }
+
+        Plotly.newPlot("bar", barChartData, barChartLayout)
+    })
+};
+
+
+//Define function for the bubble chart
+function bubbleChart(subjectID){
+    d3.json(url).then(function(jsonData){
+        let subjectData = jsonData.samples.filter(x => x.id == subjectID);
+
+
+        let bubbleChartData = [{
+            x : subjectData[0].otu_ids,
+            y : subjectData[0].sample_values,
+            text : subjectData[0].otu_labels,
+            mode : "markers",
+            marker : {
+                size : subjectData.sample_values,
+                color : subjectData.otu_ids
+            }
+        }];
+
+        let bubbleChartLayout = {
+            showlegend: false,
+            xaxis: {
+              title: {
+                text: 'OTU ID'
+            }},
+            height: 600,
+            width: 800
+          };
+
+        Plotly.newPlot("bubble", bubbleChartData, bubbleChartLayout)
+    })
+};
 
